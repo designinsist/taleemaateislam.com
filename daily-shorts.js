474869f1b6126b99fec8be411e5037fc99ae88ab
Shahid Amin Khatana <mr.shahidamin@gmail.com>
2026-07-18 09:10:53 +0500

    Sync latest YouTube updates for July 18

--- daily-shorts.js
+++ daily-shorts.js
@@ -16,6 +16,7 @@
     '0LZaZolQWy4', 'W-90BrktqxM', '6Msb1kApsMk', 'PhDivJqdc_g', 'u7GHKXT8grs',
     'QyudtBM3GtU', '5RGI2wAC6Cw', 'qt--QasbnnU', '3_kwn0fKqYc', '5VRsIUjAASY',
     'MRL_yGNvq54', '6JP-Icioorw', 'skWl42otAPA', 'iFZ27xwDgYQ', 'pEciIgBHCsY',
+    '-_U00xdWMKw',
   ];
 
   function buildEmbedUrl(videoId) {
